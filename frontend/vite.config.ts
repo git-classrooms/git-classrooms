7770d01b4a417b49e3f97d4ae3c93b5f286553fd
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2024-01-29 20:48:22 +0100

    feature(frontend): Implement basic tanstack router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: true,
-        proxy: {
-            '/api/*': 'http://localhost:3000',
-        }
-    }
-})
+  plugins: [
+    react(),
+    TanStackRouterVite({
+      routeFileIgnorePrefix: "-",
+      quoteStyle: "double",
+    }),
+  ],
+  server: {
+    host: true,
+    proxy: {
+      "/api": "http://localhost:3000",
+      "/auth": "http://localhost:3000",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
