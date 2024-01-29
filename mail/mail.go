87b4c2c60adbaad9d46c5e571fa9857e5dc33f7e
Hauke Ingwersen <hauke.ingwersen@stud.hs-flensburg.de>
2024-01-29 19:33:49 +0100

    Use mail config in mail package

--- mail/mail.go
+++ mail/mail.go
@@ -1,7 +1,7 @@
 package mail
 
 import (
-	"backend/config"
+	mailConfig "backend/config/mail"
 	"bytes"
 	"crypto/tls"
 	"html/template"
@@ -18,12 +18,7 @@ func New(to, subject string) Mail {
 	return Mail{to, subject}
 }
 
-func (m *Mail) Send(templateFileName string, data interface{}) error {
-	cfg, err := mailConfig()
-	if err != nil {
-		return err
-	}
-
+func (m *Mail) Send(config *mailConfig.Config, templateFileName string, data interface{}) error {
 	t, err := template.ParseFiles(templateFileName)
 
 	if err != nil {
@@ -38,7 +33,7 @@ func (m *Mail) Send(templateFileName string, data interface{}) error {
 	result := tpl.String()
 
 	mail := gomail.NewMessage()
-	mail.SetHeader("From", cfg.user)
+	mail.SetHeader("From", config.User)
 	mail.SetHeader("To", m.to)
 	mail.SetHeader("Subject", m.subject)
 	mail.SetBody("text/html", result)
@@ -46,7 +41,7 @@ func (m *Mail) Send(templateFileName string, data interface{}) error {
 	// This needs to be updated when
 	// https://gitlab.hs-flensburg.de/fb3-masterprojekt-gitlab-classroom/gitlab-classroom/-/merge_requests/5
 	// is merged
-	dailer := gomail.NewDialer(cfg.host, cfg.port, cfg.user, cfg.password)
+	dailer := gomail.NewDialer(config.Host, config.Port, config.User, config.Password)
 	dailer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
 	return dailer.DialAndSend(mail)
