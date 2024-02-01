fbda5a10723d8e613c4aca44503e0a4cdfefbe45
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2024-02-01 12:27:20 +0100

    feat: do not add members directly to the group and create our db object if successfull on gitlab

--- controller/api/default_controller/create_classroom.go
+++ controller/api/default_controller/create_classroom.go
@@ -1,8 +1,12 @@
 package default_controller
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"gitlab.hs-flensburg.de/gitlab-classroom/context"
+	"gitlab.hs-flensburg.de/gitlab-classroom/context/session"
+	"gitlab.hs-flensburg.de/gitlab-classroom/model/database"
+	"gitlab.hs-flensburg.de/gitlab-classroom/model/database/query"
 	"gitlab.hs-flensburg.de/gitlab-classroom/repository/gitlab/model"
 	"net/http"
 )
@@ -13,33 +17,52 @@ type CreateClassroomRequest struct {
 	Description  string   `json:"description"`
 }
 
-func (handler *DefaultController) CreateClassroom(c *fiber.Ctx) error { //TODO: Rework, group-access-token and persisting data is needed
+func (r CreateClassroomRequest) isValid() bool {
+	return r.Name != "" && r.Description != "" && len(r.MemberEmails) == 0
+}
+
+func (handler *DefaultController) CreateClassroom(c *fiber.Ctx) error {
 	repo := context.GetGitlabRepository(c)
+	user, err := session.Get(c).GetUser()
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
 
-	var err error
 	requestBody := &CreateClassroomRequest{}
-
 	err = c.BodyParser(requestBody)
 	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	if !requestBody.isValid() {
+		return fiber.NewError(fiber.StatusBadRequest, "invalid request body")
 	}
 
 	group, err := repo.CreateGroup(
 		requestBody.Name,
 		model.Private,
 		requestBody.Description,
-		requestBody.MemberEmails, //TODO: User shouldn't be added in advance. They should be invited and have the chance not to accept
 	)
 	if err != nil {
 		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
 
-	for _, memberEmail := range requestBody.MemberEmails {
-		err = repo.CreateGroupInvite(group.ID, memberEmail)
-		if err != nil {
-			return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	classroomQuery := query.Classroom
+	classRoom := &database.Classroom{
+		Name:        requestBody.Name,
+		OwnerID:     user.ID,
+		Description: "",
+		GroupID:     group.ID,
+	}
+	err = classroomQuery.WithContext(c.Context()).Create(classRoom)
+	if err != nil {
+		newErr := repo.DeleteGroup(group.ID)
+		if newErr != nil {
+			return fiber.NewError(fiber.StatusInternalServerError, newErr.Error())
 		}
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
 
+	c.Set("Location", fmt.Sprintf("/api/v1/classrooms/%s", classRoom.ID.String()))
 	return c.SendStatus(http.StatusCreated)
 }
