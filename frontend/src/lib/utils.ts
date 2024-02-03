686a3a44074dcc31f4d6ad55a1867bdc525be079
Hauke Ingwersen <hauke.ingwersen@stud.hs-flensburg.de>
2024-02-03 12:45:06 +0100

    feat: Add isAuthenticated util function

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,19 @@
+import axios from "axios"
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function isAuthenticated() {
+  try {
+    await axios
+      .get("/api/me", { withCredentials: true })
+
+    return true
+  } catch (e) {
+    return false
+  }
+
 }
