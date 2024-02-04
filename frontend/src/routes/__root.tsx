fd9672ef7badcbc0bdf2cbd67d1853639f84f0e1
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2024-02-04 18:55:33 +0100

    feat: don't add router-devtools in production builds

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,13 +1,25 @@
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient } from "@tanstack/react-query";
+import React, { Suspense } from "react";
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;
 }>()({
   component: RootComponent,
 });
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      );
 
 function RootComponent() {
   return (
@@ -16,7 +28,9 @@ function RootComponent() {
         <div className="max-w-2xl m-auto">
           <Outlet />
           <ReactQueryDevtools initialIsOpen={false} />
-          <TanStackRouterDevtools />
+          <Suspense>
+            <TanStackRouterDevtools />
+          </Suspense>
         </div>
       </div>
     </>
