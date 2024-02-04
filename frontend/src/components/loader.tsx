32354dfc0aea93a608dc8ada297cbb76e164a0c4
Hauke Ingwersen <hauke.ingwersen@stud.hs-flensburg.de>
2024-02-04 14:07:28 +0100

    refactor: Remove newline

--- frontend/src/components/loader.tsx
+++ frontend/src/components/loader.tsx
@@ -1,7 +1,6 @@
 import { Loader2 } from "lucide-react";
 
 export function Loader() {
-
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <Loader2 className="animate-spin h-8 w-8" />
