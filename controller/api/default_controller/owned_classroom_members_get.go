c290e5df126dc77282bd7e75c6bce2a6d18bd64b
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2024-04-12 10:26:29 +0200

    feat: Add a new endpoint definition

--- controller/api/default_controller/owned_classroom_members_get.go
+++ controller/api/default_controller/owned_classroom_members_get.go
@@ -6,6 +6,15 @@ import (
 	"gitlab.hs-flensburg.de/gitlab-classroom/wrapper/context"
 )
 
+// @Summary		Get classroom Members
+// @Description	Create a new classroom
+// @Tags			classroom
+// @Produces		json
+// @Param			classroomId	path		string	true	"Classroom ID" Format(uuid)
+// @Success		200			{array}		database.User
+// @Failure		401			{object}	httputil.HTTPError
+// @Failure		500			{object}	httputil.HTTPError
+// @Router			/classrooms/owned/{classroomId}/members [get]
 func (ctrl *DefaultController) GetOwnedClassroomMembers(c *fiber.Ctx) error {
 	ctx := context.Get(c)
 	classroom := ctx.GetOwnedClassroom()
