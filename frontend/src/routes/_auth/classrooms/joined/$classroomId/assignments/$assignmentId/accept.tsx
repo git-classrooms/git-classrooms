06720029e3e2abeb4a3a3a5f7a43aae518e49360
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2024-05-12 11:17:57 +0200

    refactor: Change to seperator and make things bold

--- frontend/src/routes/_auth/classrooms/joined/$classroomId/assignments/$assignmentId/accept.tsx
+++ frontend/src/routes/_auth/classrooms/joined/$classroomId/assignments/$assignmentId/accept.tsx
@@ -6,16 +6,19 @@ import { joinedClassroomAssignmentQueryOptions, useAcceptAssignment } from "@/ap
 import { Link } from "@tanstack/react-router";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { joinedClassroomQueryOptions } from "@/api/classrooms.ts";
+import { Separator } from "@/components/ui/separator";
 
-export const Route = createFileRoute('/_auth/classrooms/joined/$classroomId/assignments/$assignmentId/accept')({
-  loader: async ({context, params}) => {
-    const assignment = await context.queryClient.ensureQueryData(joinedClassroomAssignmentQueryOptions(params.classroomId, params.assignmentId))
-    return {assignment}
+export const Route = createFileRoute("/_auth/classrooms/joined/$classroomId/assignments/$assignmentId/accept")({
+  loader: async ({ context, params }) => {
+    const assignment = await context.queryClient.ensureQueryData(
+      joinedClassroomAssignmentQueryOptions(params.classroomId, params.assignmentId),
+    );
+    return { assignment };
   },
-  component: AcceptAssignment
-})
+  component: AcceptAssignment,
+});
 
-function AcceptAssignment(){
+function AcceptAssignment() {
   const navigate = useNavigate({
     from: "/_auth/classrooms/$classroomId/assignments/$assignmentId/accept/",
   });
@@ -23,44 +26,46 @@ function AcceptAssignment(){
   const { data: classroom } = useSuspenseQuery(joinedClassroomQueryOptions(classroomId));
   const { data: assignemnt } = useSuspenseQuery(joinedClassroomAssignmentQueryOptions(classroomId, assignmentId));
   const { mutateAsync, isError, isPending } = useAcceptAssignment(classroomId, assignmentId);
-  const onClick = async ()=>{
-    await mutateAsync()
+  const onClick = async () => {
+    await mutateAsync();
     await navigate({ to: "/classrooms" });
-  }
+  };
 
-  return(
-    <div className="p-6 rounded-lg border">
-      <h1 className="text-5xl font-bold text-center">Accept Assignment</h1>
-      <div className="divide-y divide-solid">
-        <div className="py-6">
-          <p className="text-slate-500 text-lg">
-            You need to accept the assignment <span>{assignemnt.assignment.name}</span> in the classroom <span>{classroom.classroom.name}</span>.
-          </p>
-        </div>
-        <div className="py-6">
-          <p className="text-slate-500 text-lg">
-            Once you have accepted the assignment, you will get access to the repository <span>{assignemnt.assignment.name}</span>.
-            in the <span>{classroom.classroom.name}</span> group.
-          </p>
-        </div>
-        <div className="pt-6 flex justify-between">
-          <Button variant="destructive" asChild>
-            <Link to="/classrooms/joined/$classroomId" params={{ classroomId }} property='stylesheet'>Reject</Link>
-          </Button>
-          <Button onClick={onClick} disabled={isPending}>
-            {isPending ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : "Accept"}
-          </Button>
-        </div>
+  return (
+    <div className="p-6 rounded-lg border flex flex-col gap-5">
+      <h1 className="text-5xl font-bold text-center mb-5">Accept Assignment</h1>
+      <Separator />
+      <p className="text-slate-500">
+        You need to accept the assignment <span className="font-bold">{assignemnt.assignment.name}</span> in the
+        classroom <span className="font-bold">{classroom.classroom.name}</span>.
+      </p>
+      <Separator />
+      <p className="text-slate-500">
+        Once you have accepted the assignment, you will get access to the repository{" "}
+        <span>{assignemnt.assignment.name}</span>. in the <span>{classroom.classroom.name}</span> group.
+      </p>
+      <Separator />
+      <div className="flex justify-between">
+        <Button variant="destructive" asChild>
+          <Link to="/classrooms/joined/$classroomId" params={{ classroomId }} property="stylesheet">
+            Reject
+          </Link>
+        </Button>
+        <Button onClick={onClick} disabled={isPending}>
+          {isPending ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : "Accept"}
+        </Button>
       </div>
 
       {isError && (
-        <Alert variant="destructive">
-          <AlertCircle className="h-4 w-4" />
-          <AlertTitle>Error</AlertTitle>
-          <AlertDescription>Can't accept assignment!</AlertDescription>
-        </Alert>
+        <>
+          <Separator />
+          <Alert variant="destructive">
+            <AlertCircle className="h-4 w-4" />
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>Can't accept assignment!</AlertDescription>
+          </Alert>
+        </>
       )}
     </div>
   );
-
 }
--- frontend/src/routes/_auth/classrooms/joined/$classroomId/invitations/$invitationId.tsx
+++ frontend/src/routes/_auth/classrooms/joined/$classroomId/invitations/$invitationId.tsx
@@ -1,15 +1,16 @@
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { Button } from "@/components/ui/button.tsx";
 import { AlertCircle, Loader2 } from "lucide-react";
-import { invitationInfoQueryOptions, useJoinClassroom } from "@/api/classrooms.ts";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert.tsx";
+import { invitationInfoQueryOptions, useJoinClassroom } from "@/api/classrooms";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Separator } from "@/components/ui/separator";
 import { getUUIDFromLocation } from "@/lib/utils";
 import { Action } from "@/swagger-client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 export const Route = createFileRoute("/_auth/classrooms/joined/$classroomId/invitations/$invitationId")({
-  loader: async({context, params}) => {
-    const invitationInfo = await context.queryClient.ensureQueryData(invitationInfoQueryOptions(params.invitationId))
+  loader: async ({ context, params }) => {
+    const invitationInfo = await context.queryClient.ensureQueryData(invitationInfoQueryOptions(params.invitationId));
     return { invitationInfo };
   },
   component: JoinClassroom,
@@ -27,41 +28,40 @@ function JoinClassroom() {
     await navigate({ to: "/classrooms/joined/$classroomId", params: { classroomId } });
   };
 
-  const onReject= async () => {
+  const onReject = async () => {
     await mutateAsync(Action.Reject);
     await navigate({ to: "/classrooms" });
   };
 
   return (
-    <div className="p-6 rounded-lg  border">
-      <h1 className="text-5xl font-bold text-center">Join Classroom</h1>
-      <div className="divide-y divide-solid">
-        <div className="py-6">
-          <p className="text-slate-500 text-lg">
-            You have been invited to join the classroom <span>{data.classroom.name}</span> by <span>{data.classroom.owner.name}</span>
-          </p>
-        </div>
-        <div className="py-6">
-          <p className="text-slate-500 text-lg">
-            {data.classroom.description}
-          </p>
-        </div>
-        <div className="pt-6 flex justify-between">
-          <Button onClick={onReject} variant="destructive" disabled={isPending}>
-            {isPending ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : "Reject"}
-          </Button>
-          <Button onClick={onAccept} disabled={isPending}>
-            {isPending ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : "Accept"}
-          </Button>
-        </div>
-        {isError && (
+    <div className="p-6 rounded-lg border flex flex-col gap-5">
+      <h1 className="text-5xl font-bold text-center mb-5">Join Classroom</h1>
+      <Separator />
+      <p className="text-slate-500">
+        You have been invited to join the classroom <span className="font-bold">{data.classroom.name}</span> by{" "}
+        <span className="font-bold">{data.classroom.owner.name}</span>
+      </p>
+      <Separator />
+      <p className="text-slate-500">{data.classroom.description}</p>
+      <Separator />
+      <div className="flex justify-between">
+        <Button onClick={onReject} variant="destructive" disabled={isPending}>
+          {isPending ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : "Reject"}
+        </Button>
+        <Button onClick={onAccept} disabled={isPending}>
+          {isPending ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : "Accept"}
+        </Button>
+      </div>
+      {isError && (
+        <>
+          <Separator />
           <Alert variant="destructive">
             <AlertCircle className="h-4 w-4" />
             <AlertTitle>Error</AlertTitle>
             <AlertDescription>Can't join classroom!</AlertDescription>
           </Alert>
-        )}
-      </div>
+        </>
+      )}
     </div>
   );
 }
