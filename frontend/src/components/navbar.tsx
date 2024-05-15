0c5eec879970e1408f4267b3c9bcff080dce28de
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2024-05-15 16:12:57 +0200

    feat: Make switch keyboard accessible

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,7 +1,7 @@
 import GitlabLogo from "@/assets/gitlab_logo.svg";
-import { ModeToggle } from "@/components/modeToggle.tsx";
+import { ModeToggle } from "@/components/modeToggle";
 import { Link } from "@tanstack/react-router";
-import { useCsrf } from "@/provider/csrfProvider.tsx";
+import { useCsrf } from "@/provider/csrfProvider";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,14 +10,22 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { LogOut, User as UserIcon } from "lucide-react";
+import { LogOut, Moon, Sun, User as UserIcon } from "lucide-react";
 import { Avatar } from "./avatar";
 import { GetMeResponse } from "@/swagger-client";
-import { Button } from "@/components/ui/button.tsx";
-
+import { Button } from "@/components/ui/button";
+import { useTheme } from "@/provider/themeProvider";
+import { useEffect, useState } from "react";
+import { Switch } from "@/components/ui/switch";
 
 export function Navbar(props: { auth: GetMeResponse | null }) {
   const { csrfToken } = useCsrf();
+  const { theme, setTheme } = useTheme();
+  const [isDarkMode, setIsDarkMode] = useState(theme === "dark");
+
+  useEffect(() => {
+    setTheme(isDarkMode ? "dark" : "light");
+  }, [isDarkMode]);
 
   return (
     <nav className="flex justify-between px-8 py-2.5 mb-8 border-b">
@@ -55,11 +63,12 @@ export function Navbar(props: { auth: GetMeResponse | null }) {
               </Link>
             </li>
           </ul>
-        ) : (<div />)}
+        ) : (
+          <div />
+        )}
       </div>
       <div className="flex items-center">
-        <div className="px-4 py-2">
-        </div>
+        <div className="px-4 py-2"></div>
         {props.auth ? (
           <DropdownMenu>
             <DropdownMenuTrigger>
@@ -72,28 +81,38 @@ export function Navbar(props: { auth: GetMeResponse | null }) {
             <DropdownMenuContent>
               <DropdownMenuLabel>
                 <div className="font-medium">{props.auth.name}</div>
-                <div className="text-sm text-muted-foreground md:inline">
-                  @{props.auth.gitlabUsername}
-                </div>
+                <div className="text-sm text-muted-foreground md:inline">@{props.auth.gitlabUsername}</div>
               </DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <a href={props.auth.gitlabUrl} target="_blank" rel="noopener noreferrer" className="flex items-center w-full">
-                <DropdownMenuItem className="flex items-center w-full">
+              <a
+                href={props.auth.gitlabUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center w-full"
+              >
+                <DropdownMenuItem className="w-full">
                   <UserIcon className="mr-2 h-4 w-4" />
                   <span>Profile</span>
                 </DropdownMenuItem>
               </a>
-              <DropdownMenuItem onSelect={event => event.preventDefault()}>
-                <ModeToggle />
+
+              <DropdownMenuItem
+                onClick={() => setIsDarkMode((prev) => !prev)}
+                onSelect={(event) => event.preventDefault()}
+              >
+                <Sun className="h-[1.2rem] w-[1.2rem] mr-2 dark:text-white text-black" />
+                <Switch checked={isDarkMode} />
+                <Moon className="h-[1.2rem] w-[1.2rem] ml-2 dark:text-white text-black" />
+                <span className="sr-only">Toggle theme</span>
               </DropdownMenuItem>
               <DropdownMenuSeparator />
 
-              <form id="logOutForm" method="POST" action="/api/v1/auth/sign-out" className="w-full">
-                <button type="submit" className="font-bold w-full">
-                  <DropdownMenuItem>
+              <form id="logOutForm" method="POST" action="/api/v1/auth/sign-out">
+                <button type="submit" className="w-full">
+                  <DropdownMenuItem className="w-full">
                     <LogOut className="mr-2 h-4 w-4" />
                     <input type="hidden" name="csrf_token" value={csrfToken} />
-                    <span className="">Log out</span>
+                    <span className="font-bold">Log out</span>
                   </DropdownMenuItem>
                 </button>
               </form>
