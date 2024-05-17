94c7842b43d42d8bfafa9740a34e95754832c8ea
Jannes <jane1220@stud.hs-flensburg.de>
2024-05-17 09:57:02 +0200

    implemented PutOwnedAssignments

--- controller/api/default_controller/owned_classroom_assignments_put.go
+++ controller/api/default_controller/owned_classroom_assignments_put.go
@@ -4,6 +4,8 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"gitlab.hs-flensburg.de/gitlab-classroom/model/database/query"
+	"gitlab.hs-flensburg.de/gitlab-classroom/wrapper/context"
 )
 
 type updateAssignmentRequest struct {
@@ -12,6 +14,70 @@ type updateAssignmentRequest struct {
 	DueDate     *time.Time `json:"dueDate,omitempty"`
 }
 
+func (r updateAssignmentRequest) isValid() bool {
+	return r.Name != "" || r.Description != "" || r.DueDate != nil
+}
+
 func (ctrl *DefaultController) PutOwnedAssignments(c *fiber.Ctx) error {
-	return nil
+	ctx := context.Get(c)
+	assignment := ctx.GetOwnedClassroomAssignment()
+	oldName := assignment.Name
+	oldDescription := assignment.Description
+
+	repo := ctx.GetGitlabRepository()
+
+	requestBody := &updateAssignmentRequest{}
+	err := c.BodyParser(requestBody)
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	if !requestBody.isValid() {
+		return fiber.NewError(fiber.StatusBadRequest, "request requires name, description or dueDate")
+	}
+
+	if requestBody.Name != "" {
+		for _, project := range assignment.Projects {
+			_, err := repo.ChangeProjectName(project.ProjectID, requestBody.Name)
+			if err != nil {
+				return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+			}
+
+			defer func(projectId int) {
+				if recover() != nil || err != nil {
+					repo.ChangeProjectName(projectId, oldName)
+				}
+			}(project.ProjectID)
+		}
+
+		assignment.Name = requestBody.Name
+	}
+
+	if requestBody.Description != "" {
+		for _, project := range assignment.Projects {
+			_, err := repo.ChangeProjectDescription(project.ProjectID, requestBody.Description)
+			if err != nil {
+				return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+			}
+
+			defer func(projectId int) {
+				if recover() != nil || err != nil {
+					repo.ChangeGroupDescription(projectId, oldDescription)
+				}
+			}(project.ProjectID)
+		}
+
+		assignment.Description = requestBody.Description
+	}
+
+	if requestBody.DueDate != nil {
+		assignment.DueDate = requestBody.DueDate
+	}
+
+	if _, err := query.Assignment.WithContext(c.Context()).Updates(assignment); err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	c.SendStatus(fiber.StatusAccepted)
+	return c.JSON(assignment)
 }
--- controller/api/default_controller/owned_classroom_assignments_put_test.go
+++ controller/api/default_controller/owned_classroom_assignments_put_test.go
@@ -3,7 +3,7 @@ package default_controller
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"testing"
 	"time"
 
@@ -31,7 +31,7 @@ func TestPutOwnedAssignments(t *testing.T) {
 		OwnerID: user.ID,
 	}
 	testDb.InsertClassroom(&classroom)
-	oldTime := time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+	oldTime := time.Date(1970, 1, 1, 0, 0, 0, 0, time.Local)
 	assignment := database.Assignment{
 		ID:          uuid.New(),
 		Name:        "Test",
@@ -53,6 +53,7 @@ func TestPutOwnedAssignments(t *testing.T) {
 		ProjectID:    1,
 	}
 	testDb.InsertAssignmentProjects(&project)
+	assignment.Projects = append(assignment.Projects, &project)
 
 	gitlabRepo := gitlabRepoMock.NewMockRepository(t)
 	mailRepo := mailRepoMock.NewMockRepository(t)
@@ -60,7 +61,7 @@ func TestPutOwnedAssignments(t *testing.T) {
 	app := fiber.New()
 	app.Use("/api", func(c *fiber.Ctx) error {
 		ctx := contextWrapper.Get(c)
-		ctx.SetOwnedClassroom(&classroom)
+		ctx.SetOwnedClassroomAssignment(&assignment)
 		ctx.SetGitlabRepository(gitlabRepo)
 
 		s := session.Get(c)
@@ -71,12 +72,12 @@ func TestPutOwnedAssignments(t *testing.T) {
 	})
 
 	handler := NewApiController(mailRepo)
-	app.Put("/api/classrooms/owned/:classroomId/assignments", handler.PutOwnedAssignments)
+	app.Put("/api/classrooms/owned/:classroomId/assignments/:assignmentId", handler.PutOwnedAssignments)
 
 	targetRoute := fmt.Sprintf("/api/classrooms/owned/%s/assignments/%s", classroom.ID.String(), assignment.ID.String())
 
 	t.Run("updates assignment", func(t *testing.T) {
-		newTime := time.Date(2000, 2, 2, 2, 2, 2, 2, time.UTC)
+		newTime := time.Date(2000, 2, 2, 2, 2, 2, 0, time.Local)
 		requestBody := updateAssignmentRequest{
 			Name:        "New",
 			Description: "new",
@@ -119,12 +120,13 @@ func TestPutOwnedAssignments(t *testing.T) {
 		assert.Equal(t, fiber.StatusAccepted, resp.StatusCode)
 		assert.NoError(t, err)
 
+		gitlabRepo.AssertExpectations(t)
+
 		updatedAssignment, err := query.Assignment.WithContext(context.Background()).Where(query.Assignment.ID.Eq(assignment.ID)).First()
 		assert.NoError(t, err)
 		assert.Equal(t, requestBody.Name, updatedAssignment.Name)
 		assert.Equal(t, requestBody.Description, updatedAssignment.Description)
 		assert.Equal(t, newTime, *updatedAssignment.DueDate)
-
 	})
 
 	t.Run("request body is empty", func(t *testing.T) {
@@ -136,12 +138,12 @@ func TestPutOwnedAssignments(t *testing.T) {
 		assert.Equal(t, fiber.StatusBadRequest, resp.StatusCode)
 		assert.NoError(t, err)
 
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		bodyBytes, err := io.ReadAll(resp.Body)
 		if err != nil {
 			t.Fatal(err)
 		}
 		bodyString := string(bodyBytes)
-		assert.Equal(t, "request body is empty", bodyString)
+		assert.Equal(t, "request requires name, description or dueDate", bodyString)
 	})
 
 	t.Run("gitlab error on change project name", func(t *testing.T) {
@@ -149,11 +151,31 @@ func TestPutOwnedAssignments(t *testing.T) {
 			Name: "New",
 		}
 
-		errorMsg := "gitlab error"
+		project2 := database.AssignmentProjects{
+			AssignmentID: assignment.ID,
+			TeamID:       team.ID,
+			ProjectID:    2,
+		}
+		testDb.InsertAssignmentProjects(&project2)
+		assignment.Projects = append(assignment.Projects, &project2)
+
 		gitlabRepo.
 			EXPECT().
 			ChangeProjectName(
 				project.ProjectID,
+				requestBody.Name,
+			).
+			Return(
+				&model.Project{},
+				nil,
+			).
+			Times(1)
+
+		errorMsg := "gitlab error"
+		gitlabRepo.
+			EXPECT().
+			ChangeProjectName(
+				project2.ProjectID,
 				requestBody.Name,
 			).
 			Return(
@@ -162,28 +184,15 @@ func TestPutOwnedAssignments(t *testing.T) {
 			).
 			Times(1)
 
-		req := newPutJsonRequest(targetRoute, requestBody)
-		resp, err := app.Test(req)
-
-		assert.NoError(t, err)
-		assert.Equal(t, fiber.StatusInternalServerError, resp.StatusCode)
-	})
-
-	t.Run("gitlab error on change project description", func(t *testing.T) {
-		requestBody := updateAssignmentRequest{
-			Description: "new",
-		}
-
-		errorMsg := "gitlab error"
 		gitlabRepo.
 			EXPECT().
-			ChangeProjectDescription(
+			ChangeProjectName(
 				project.ProjectID,
-				requestBody.Description,
+				assignment.Name,
 			).
 			Return(
+				&model.Project{},
 				nil,
-				fmt.Errorf(errorMsg),
 			).
 			Times(1)
 
@@ -192,6 +201,79 @@ func TestPutOwnedAssignments(t *testing.T) {
 
 		assert.NoError(t, err)
 		assert.Equal(t, fiber.StatusInternalServerError, resp.StatusCode)
+
+		gitlabRepo.AssertExpectations(t)
+
+		updatedAssignment, err := query.Assignment.WithContext(context.Background()).Where(query.Assignment.ID.Eq(assignment.ID)).First()
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, assignment.Name, updatedAssignment.Name)
+	})
+
+	t.Run("gitlab error on change project description", func(t *testing.T) {
+		requestBody := updateAssignmentRequest{
+			Description: "new",
+		}
+
+		project2 := database.AssignmentProjects{
+			AssignmentID: assignment.ID,
+			TeamID:       team.ID,
+			ProjectID:    2,
+		}
+		testDb.InsertAssignmentProjects(&project2)
+		assignment.Projects = append(assignment.Projects, &project2)
+
+		gitlabRepo.
+			EXPECT().
+			ChangeProjectName(
+				project.ProjectID,
+				requestBody.Name,
+			).
+			Return(
+				&model.Project{},
+				nil,
+			).
+			Times(1)
+
+		errorMsg := "gitlab error"
+		gitlabRepo.
+			EXPECT().
+			ChangeProjectDescription(
+				project2.ProjectID,
+				requestBody.Description,
+			).
+			Return(
+				nil,
+				fmt.Errorf(errorMsg),
+			).
+			Times(1)
+
+		gitlabRepo.
+			EXPECT().
+			ChangeProjectDescription(
+				project.ProjectID,
+				assignment.Description,
+			).
+			Return(
+				&model.Project{},
+				nil,
+			).
+			Times(1)
+
+		req := newPutJsonRequest(targetRoute, requestBody)
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusInternalServerError, resp.StatusCode)
+
+		gitlabRepo.AssertExpectations(t)
+
+		updatedAssignment, err := query.Assignment.WithContext(context.Background()).Where(query.Assignment.ID.Eq(assignment.ID)).First()
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, assignment.Description, updatedAssignment.Description)
 	})
 
 }
