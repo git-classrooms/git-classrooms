7f24c00c619d245b5a5cf1af0eb40326eb1343ec
Dominik Heckner <dominik.heckner@stud.hs-flensburg.de>
2024-05-23 19:31:31 +0200

    feat: Add regex in assignment creation

--- frontend/src/types/assignments.ts
+++ frontend/src/types/assignments.ts
@@ -1,10 +1,13 @@
 import { z } from "zod";
 
 export const createAssignmentFormSchema = z.object({
-  name: z.string().min(3),
+  name: z.string()
+    .min(3)
+    .regex(/^[\p{L}\p{N}\p{Emoji}_.+\-\s]+$/u, "Invalid characters in name"),
   description: z.string().min(3),
   templateProjectId: z.number().min(1, "Please select a template project"),
   dueDate: z.coerce.date(),
 });
+
 
 export type CreateAssignmentForm = z.infer<typeof createAssignmentFormSchema>;
