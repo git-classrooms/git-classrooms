8d526d28fe13c5de751e8d3b372eb4fc093b4a91
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2024-05-25 22:28:00 +0000

    fix: Allow empty dueDate in the frontend

--- frontend/src/types/assignments.ts
+++ frontend/src/types/assignments.ts
@@ -4,7 +4,7 @@ export const createAssignmentFormSchema = z.object({
   name: z.string().min(3),
   description: z.string().min(3),
   templateProjectId: z.number().min(1, "Please select a template project"),
-  dueDate: z.coerce.date(),
+  dueDate: z.coerce.date().optional(),
 });
 
 export type CreateAssignmentForm = z.infer<typeof createAssignmentFormSchema>;
