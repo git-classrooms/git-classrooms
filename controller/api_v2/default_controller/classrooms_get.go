6b94b68c5133f39d828eaffc7c2e7b3eb4c67ad2
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2024-05-25 19:06:40 +0200

    feat: Add filter with query param to get classrooms of sepcific roles

--- controller/api_v2/default_controller/classrooms_get.go
+++ controller/api_v2/default_controller/classrooms_get.go
@@ -2,28 +2,61 @@ package api
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"gitlab.hs-flensburg.de/gitlab-classroom/model/database"
+	"gitlab.hs-flensburg.de/gitlab-classroom/model/database/query"
 	"gitlab.hs-flensburg.de/gitlab-classroom/utils"
 	"gitlab.hs-flensburg.de/gitlab-classroom/wrapper/context"
 )
+
+type filter string //@Name Filter
+
+const (
+	ownedClassrooms     filter = "owned"
+	moderatorClassrooms filter = "moderator"
+	studentClassrooms   filter = "student"
+)
+
+type classroomRequestQuery struct {
+	Filter filter `query:"filter"`
+}
 
 // @Summary		Get classrooms
 // @Description	Get classrooms
 // @Id				GetClassrooms
 // @Tags			classroom
 // @Produce		json
-// @Success		200	{array}		api.UserClassroomResponse
-// @Failure		401	{object}	HTTPError
-// @Failure		500	{object}	HTTPError
+// @Param			filter	query		api.filter	false	"Filter Options"
+// @Success		200		{array}		api.UserClassroomResponse
+// @Failure		401		{object}	HTTPError
+// @Failure		500		{object}	HTTPError
 // @Router			/api/v2/classrooms [get]
 func (ctrl *DefaultController) GetClassrooms(c *fiber.Ctx) (err error) {
 	ctx := context.Get(c)
 	userID := ctx.GetUserID()
 
-	classrooms, err := userClassroomQuery(c, userID).
-		Find()
+	var urlQuery classroomRequestQuery
+
+	if err = c.QueryParser(&urlQuery); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	log.Println("GetClassrooms", urlQuery)
+
+	dbQuery := userClassroomQuery(c, userID)
+	switch urlQuery.Filter {
+	case ownedClassrooms:
+		dbQuery = dbQuery.Where(query.UserClassrooms.Role.Eq(uint8(database.Owner)))
+	case moderatorClassrooms:
+		dbQuery = dbQuery.Where(query.UserClassrooms.Role.Eq(uint8(database.Moderator)))
+	case studentClassrooms:
+		dbQuery = dbQuery.Where(query.UserClassrooms.Role.Eq(uint8(database.Student)))
+	default:
+	}
+
+	classrooms, err := dbQuery.Find()
 	if err != nil {
 		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
