b4335a71f7504c6e74c365394324e90bf005d386
Philipp Borucki <p.borucki@icloud.com>
2024-06-01 15:49:20 +0200

    feat: add sum and all slice functions

--- utils/map.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-func Map[TInput, TOutput any](inputArray []TInput, mapFunction func(TInput) TOutput) []TOutput {
-	outputArray := make([]TOutput, len(inputArray))
-	for i := range inputArray {
-		outputArray[i] = mapFunction(inputArray[i])
-	}
-	return outputArray
-}
