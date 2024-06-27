c60089c28c9fcf6e7d186d9e737d9adc0d17b727
Claas Thore Klein <claas-thore.klein@stud.hs-flensburg.de>
2024-06-28 00:40:28 +0200

    basic

--- frontend/src/components/activeAssignments.tsx
+++ frontend/src/components/activeAssignments.tsx
@@ -1,29 +1,42 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card.tsx";
 import { Button } from "@/components/ui/button.tsx";
-import { Link } from "@tanstack/react-router";
-import { Clipboard, Gitlab } from "lucide-react";
+import { Gitlab } from "lucide-react";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card.tsx";
+import { Separator } from "@/components/ui/separator.tsx";
 import List from "@/components/ui/list.tsx";
 import ListItem from "@/components/ui/listItem.tsx";
 import { Assignment } from "@/swagger-client";
-import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card.tsx";
-import { Separator } from "@/components/ui/separator.tsx";
 
 /**
- * AssignmentListCard is a React component that displays a list of active assignments in a classroom.
+ * AssignmentListCard is a React component that displays a list of assignments in a classroom.
+ * It includes a table of assignments.
  *
  * @param {Object} props - The properties passed to the component.
- * @param {Array} props.assignments - An array of Assignment objects representing the assignments of the classroom.
+ * @param {Array} props.assignments - An array of Assignment objects representing the assignments in the classroom.
+ * @param {boolean} props.showActiveOnly - A boolean to determine if only active assignments should be shown.
  * @returns {JSX.Element} A React component that displays a card with the list of assignments in a classroom.
  */
-export function AssignmentListCard({ assignments }: { assignments: Assignment[] }): JSX.Element {
+export function AssignmentListCard({
+  assignments,
+  showActiveOnly,
+}: {
+  assignments: Assignment[];
+  showActiveOnly: boolean;
+}): JSX.Element {
+  const filteredAssignments = showActiveOnly
+    ? assignments.filter((assignment) => !assignment.dueDate || new Date(assignment.dueDate) > new Date())
+    : assignments;
+
   return (
     <Card className="p-2">
       <CardHeader>
-        <CardTitle>Assignments</CardTitle>
-        <CardDescription>All active assignments for this classroom</CardDescription>
+        <CardTitle>{showActiveOnly ? "Active Assignments" : "Assignments"}</CardTitle>
+        <CardDescription>
+          {showActiveOnly ? "Your assignements that are not yet overdue." : "Your assignements"}
+        </CardDescription>
       </CardHeader>
       <CardContent>
-        <AssignmentTable assignments={assignments} />
+        <AssignmentTable assignments={filteredAssignments} />
       </CardContent>
     </Card>
   );
@@ -37,11 +50,19 @@ function AssignmentTable({ assignments }: { assignments: Assignment[] }) {
         <ListItem
           leftContent={<AssignmentListElement assignment={assignment} />}
           rightContent={
-            <Button variant="ghost" size="icon" asChild>
-              <a href={assignment.webUrl} target="_blank" rel="noreferrer">
-                <Gitlab className="h-6 w-6 text-gray-600" />
-              </a>
-            </Button>
+            <div className="flex text-end gap-2">
+              <div>
+                <div className="font-medium">Due Date</div>
+                <div className="text-sm text-muted-foreground">
+                  {assignment.dueDate ? new Date(assignment.dueDate).toLocaleDateString() : "No due date"}
+                </div>
+              </div>
+              <Button variant="ghost" size="icon" asChild>
+                <a href="#" target="_blank" rel="noreferrer">
+                  <Gitlab className="h-6 w-6 text-gray-600" />
+                </a>
+              </Button>
+            </div>
           }
         />
       )}
@@ -53,16 +74,22 @@ function AssignmentListElement({ assignment }: { assignment: Assignment }) {
   return (
     <HoverCard>
       <HoverCardTrigger className="cursor-default flex">
-        <div className="pr-2">
+        <div>
           <div className="font-medium">{assignment.name}</div>
           <div className="text-sm text-muted-foreground">{assignment.description}</div>
         </div>
       </HoverCardTrigger>
       <HoverCardContent className="w-100">
         <p className="text-lg font-semibold">{assignment.name}</p>
-        <p className="text-sm text-muted-foreground mt-[-0.3rem]">{assignment.description}</p>
+        <p className="text-sm text-muted-foreground mt-[-0.3rem]">
+          Created at: {new Date(assignment.createdAt).toLocaleDateString()}
+        </p>
         <Separator className="my-1" />
-        <p className="text-muted-foreground">Due Date: {assignment.dueDate ? assignment.dueDate : "No due date"}</p>
+        <p className="text-muted-foreground">{assignment.description}</p>
+        <Separator className="my-1" />
+        <div className="text-muted-foreground">
+          <span className="font-bold">Template Project ID:</span> {assignment.templateProjectId}
+        </div>
       </HoverCardContent>
     </HoverCard>
   );
