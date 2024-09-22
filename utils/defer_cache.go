87447bc57f1469984fadca9e4934aa00dc56024c
Philipp Borucki <philipp.borucki@stud.hs-flensburg.de>
2024-09-22 09:47:04 +0200

    docs: add comments for package worker utils

--- utils/defer_cache.go
+++ utils/defer_cache.go
@@ -2,6 +2,7 @@ package utils
 
 import "gitlab.hs-flensburg.de/gitlab-classroom/repository/gitlab/model"
 
+// ProjectAccessLevelCache is a cache for project access levels.
 type ProjectAccessLevelCache struct {
 	UserID      int
 	ProjectID   int
--- utils/slices.go
+++ utils/slices.go
@@ -1,5 +1,7 @@
+// Package utils provides utility functions for general-purpose functionality.
 package utils
 
+// Filter filters the elements of an array based on a predicate.
 func Filter[TInput any](inputArray []TInput, predicate func(TInput) bool) []TInput {
 	outputArray := make([]TInput, 0)
 	for i := range inputArray {
@@ -10,6 +12,7 @@ func Filter[TInput any](inputArray []TInput, predicate func(TInput) bool) []TInp
 	return outputArray
 }
 
+// Map maps the elements of an array to another array based on a mapping function.
 func Map[TInput, TOutput any](inputArray []TInput, mapFunction func(TInput) TOutput) []TOutput {
 	outputArray := make([]TOutput, len(inputArray))
 	for i := range inputArray {
@@ -18,6 +21,7 @@ func Map[TInput, TOutput any](inputArray []TInput, mapFunction func(TInput) TOut
 	return outputArray
 }
 
+// FlatMap maps the elements of an array to another array based on a mapping function that returns a slice.
 func FlatMap[TInput, TOutput any](inputArray []TInput, mapFunction func(TInput) []TOutput) []TOutput {
 	outputArray := make([]TOutput, 0)
 	for i := range inputArray {
@@ -26,6 +30,7 @@ func FlatMap[TInput, TOutput any](inputArray []TInput, mapFunction func(TInput) 
 	return outputArray
 }
 
+// Flatten flattens a 2D array to a 1D array.
 func Flatten[T any](inputArray [][]T) []T {
 	outputArray := make([]T, 0)
 	for i := range inputArray {
@@ -34,6 +39,7 @@ func Flatten[T any](inputArray [][]T) []T {
 	return outputArray
 }
 
+// Some checks if any element of an array satisfies a predicate.
 func Some[TInput any](inputArray []TInput, predicate func(TInput) bool) bool {
 	for _, e := range inputArray {
 		if predicate(e) {
@@ -43,6 +49,7 @@ func Some[TInput any](inputArray []TInput, predicate func(TInput) bool) bool {
 	return false
 }
 
+// All checks if all elements of an array satisfy a predicate.
 func All[TInput any](inputArray []TInput, predicate func(TInput) bool) bool {
 	for _, e := range inputArray {
 		if !predicate(e) {
