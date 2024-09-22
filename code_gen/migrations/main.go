6be8b407d82bbd4d10fee21c7cafbf92db20f54a
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2024-09-22 21:29:39 +0200

    feat: make the waiting for postgres better

--- code_gen/migrations/main.go
+++ code_gen/migrations/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"database/sql"
 	"fmt"
@@ -8,6 +9,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"sync"
 	"syscall"
 	"time"
@@ -24,47 +26,10 @@ import (
 )
 
 var migraImage = "supabase/migra:3.0.1663481299"
-var postgresImage = "postgres:16"
+var postgresImage = "postgres:16-alpine"
 var postgresPort = "30000"
 var postgresUser = "postgres"
 var postgresPassword = "postgres"
-
-func GetDsn(provider string) string {
-	return fmt.Sprintf("postgresql://%s:%s@localhost:%s/%s", postgresUser, postgresPassword, postgresPort, provider)
-}
-
-func GetDBConnection(provider string) (*sql.DB, error) {
-	gorm, err := gorm.Open(postgres.Open(GetDsn(provider)), &gorm.Config{})
-	if err != nil {
-		return nil, err
-	}
-	db, err := gorm.DB()
-	if err != nil {
-		return nil, err
-	}
-	return db, nil
-}
-
-func PullImage(ctx context.Context, cli *client.Client, imageName string) error {
-	log.Println("Pulling image", imageName)
-	pullResponse, err := cli.ImagePull(ctx, imageName, image.PullOptions{})
-	if err != nil {
-		return err
-	}
-	defer pullResponse.Close()
-	return nil
-}
-
-func GetLogs(ctx context.Context, cli *client.Client, contName string) (logOutput io.ReadCloser) {
-	options := container.LogsOptions{ShowStdout: true}
-
-	out, err := cli.ContainerLogs(ctx, contName, options)
-	if err != nil {
-		panic(err)
-	}
-
-	return out
-}
 
 func main() {
 	log.SetOutput(os.Stderr)
@@ -92,17 +57,9 @@ func main() {
 	log.Println("Pulled images")
 	log.Println("Starting postgres instance...")
 
-	createResponse, err := cli.ContainerCreate(ctx, &container.Config{
-		Image:        postgresImage,
-		Env:          []string{"POSTGRES_PASSWORD=postgres"},
-		ExposedPorts: nat.PortSet{"5432/tcp": struct{}{}},
-	}, &container.HostConfig{PortBindings: nat.PortMap{nat.Port("5432/tcp"): []nat.PortBinding{{HostIP: "127.0.0.1", HostPort: postgresPort}}}}, &network.NetworkingConfig{}, nil, "")
+	dbID, err := createPostgresContainer(ctx, cli, postgresImage, postgresPort)
 	if err != nil {
-		log.Fatal("failed to create container", err)
-	}
-
-	if err := cli.ContainerStart(ctx, createResponse.ID, container.StartOptions{}); err != nil {
-		log.Fatal("failed to start container", err)
+		log.Fatal("failed to create postgres container", err)
 	}
 
 	var wg sync.WaitGroup
@@ -112,7 +69,7 @@ func main() {
 	go func() {
 		defer wg.Done()
 		<-ctx.Done()
-		if cli.ContainerRemove(context.Background(), createResponse.ID, container.RemoveOptions{Force: true, RemoveVolumes: true}); err != nil {
+		if cli.ContainerRemove(context.Background(), dbID, container.RemoveOptions{Force: true, RemoveVolumes: true}); err != nil {
 			log.Println("failed to remove container", err)
 		}
 	}()
@@ -121,25 +78,14 @@ func main() {
 
 	go func() {
 		defer wg.Done()
-		db := func() *sql.DB {
-			ticker := time.NewTicker(1 * time.Second)
-			for {
-				select {
-				case <-ctx.Done():
-					return nil
-				case <-ticker.C:
-					db, err := GetDBConnection("postgres")
-					if err != nil {
-						log.Println("trying to connect to postgres", err)
-						continue
-					}
-					return db
-				}
-			}
-		}()
-
-		// connect to db and create dbs
 		defer cancel()
+		defer func() { exitCodeChan <- 1 }()
+		// connect to db and create dbs
+		db, err := WaitForDBConnection(ctx, cli, dbID)
+		if err != nil {
+			log.Println("failed to connect to db", err)
+			return
+		}
 
 		if _, err = db.Exec("CREATE DATABASE gorm;"); err != nil {
 			log.Println("failed to create database", err)
@@ -156,27 +102,12 @@ func main() {
 
 		wg.Go(func() error {
 			// Connect gorm to the postgres container on the first db
-			gormDB, err := gorm.Open(postgres.Open(GetDsn("gorm")), &gorm.Config{})
-			if err != nil {
-				return err
-			}
-			db, err := gormDB.DB()
-			if err != nil {
-				return err
-			}
-			if err = database.DatabaseStatus(db); err != nil {
-				return err
-			}
-			return MigrateDatabase(gormDB)
+			return MigrateGormDatabase()
 		})
 
 		wg.Go(func() error {
 			// Connect goose to the second postgres container
-			gooseDB, err := GetDBConnection("goose")
-			if err != nil {
-				return err
-			}
-			return database.MigrateDatabase(gooseDB)
+			return MigrateGooseDatabase()
 		})
 
 		if err := wg.Wait(); err != nil {
@@ -186,42 +117,46 @@ func main() {
 		log.Println("migrated databases")
 
 		// docker run -it --rm --network host supabase/migra:3.0.1663481299 migra postgresql://postgres:postgres@localhost:port/goose postgresql://postgres:postgres@localhost:port/postgres
-		createResponse, err := cli.ContainerCreate(ctx, &container.Config{
-			Image: migraImage,
-			Cmd:   []string{"migra", "--with-privileges", "--unsafe", GetDsn("goose"), GetDsn("gorm")},
-			Tty:   true,
-		}, &container.HostConfig{NetworkMode: network.NetworkHost, AutoRemove: true}, &network.NetworkingConfig{}, nil, "")
+		logOutput, statusCode, err := runMigraContainer(ctx, cli, migraImage)
 		if err != nil {
-			log.Println("failed to create container", err)
-			return
-		}
-
-		finishChan, errChan := cli.ContainerWait(ctx, createResponse.ID, container.WaitConditionNextExit)
-
-		if err := cli.ContainerStart(ctx, createResponse.ID, container.StartOptions{}); err != nil {
-			log.Println("failed to start container", err)
-			return
-		}
-
-		select {
-		case waitReponse := <-finishChan:
-			exitCodeChan <- int(waitReponse.StatusCode)
-			if waitReponse.StatusCode == 0 {
-				log.Println("No changes detected")
-				return
-			}
-			logOutput := GetLogs(ctx, cli, createResponse.ID)
-			log.Println("Migration changes:")
-
-			io.Copy(os.Stdout, logOutput)
-		case err := <-errChan:
 			log.Println("failed to run migra", err)
+			return
 		}
+
+		if logOutput != nil {
+			defer logOutput.Close()
+			io.Copy(os.Stdout, logOutput)
+		}
+
+		exitCodeChan <- statusCode
 	}()
 
 	exitCode := <-exitCodeChan
 	wg.Wait()
 	os.Exit(exitCode)
+}
+
+func MigrateGooseDatabase() error {
+	gooseDB, err := GetDBConnection("goose")
+	if err != nil {
+		return err
+	}
+	return database.MigrateDatabase(gooseDB)
+}
+
+func MigrateGormDatabase() error {
+	gormDB, err := gorm.Open(postgres.Open(GetDsn("gorm")), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	db, err := gormDB.DB()
+	if err != nil {
+		return err
+	}
+	if err = database.DatabaseStatus(db); err != nil {
+		return err
+	}
+	return MigrateDatabase(gormDB)
 }
 
 func MigrateDatabase(db *gorm.DB) error {
@@ -240,4 +175,121 @@ func MigrateDatabase(db *gorm.DB) error {
 		&database.ManualGradingResult{},
 		&database.AssignmentJunitTest{},
 	)
+}
+
+func GetDsn(provider string) string {
+	return fmt.Sprintf("postgresql://%s:%s@localhost:%s/%s", postgresUser, postgresPassword, postgresPort, provider)
+}
+
+func GetDBConnection(provider string) (*sql.DB, error) {
+	gorm, err := gorm.Open(postgres.Open(GetDsn(provider)), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	db, err := gorm.DB()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func WaitForDBConnection(ctx context.Context, cli *client.Client, containerID string) (*sql.DB, error) {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	out, err := cli.ContainerLogs(ctx, containerID, container.LogsOptions{ShowStdout: true, Follow: true})
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(out)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), "database system is ready to accept connections") {
+			break
+		}
+	}
+	out.Close()
+
+	<-time.After(1000 * time.Millisecond)
+
+	db, err := GetDBConnection("postgres")
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func PullImage(ctx context.Context, cli *client.Client, imageName string) error {
+	log.Println("Pulling image", imageName)
+	pullResponse, err := cli.ImagePull(ctx, imageName, image.PullOptions{})
+	if err != nil {
+		return err
+	}
+	io.Copy(io.Discard, pullResponse)
+	defer pullResponse.Close()
+	return nil
+}
+
+func GetLogs(ctx context.Context, cli *client.Client, contName string) (logOutput io.ReadCloser) {
+	options := container.LogsOptions{ShowStdout: true}
+
+	out, err := cli.ContainerLogs(ctx, contName, options)
+	if err != nil {
+		panic(err)
+	}
+
+	return out
+}
+
+func createPostgresContainer(ctx context.Context, cli *client.Client, postgresImage string, postgresPort string) (string, error) {
+	createResponse, err := cli.ContainerCreate(ctx, &container.Config{
+		Image:        postgresImage,
+		Env:          []string{"POSTGRES_PASSWORD=postgres"},
+		ExposedPorts: nat.PortSet{"5432/tcp": struct{}{}},
+		Tty:          true,
+	}, &container.HostConfig{PortBindings: nat.PortMap{nat.Port("5432/tcp"): []nat.PortBinding{{HostIP: "127.0.0.1", HostPort: postgresPort}}}}, &network.NetworkingConfig{}, nil, "")
+	if err != nil {
+		return "", err
+	}
+
+	if err := cli.ContainerStart(ctx, createResponse.ID, container.StartOptions{}); err != nil {
+		return "", err
+	}
+
+	return createResponse.ID, nil
+}
+
+func runMigraContainer(ctx context.Context, cli *client.Client, migraImage string) (logOutput io.ReadCloser, statusCode int, err error) {
+	statusCode = 1
+	createResponse, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: migraImage,
+		Cmd:   []string{"migra", "--with-privileges", "--unsafe", GetDsn("goose"), GetDsn("gorm")},
+		Tty:   true,
+	}, &container.HostConfig{NetworkMode: network.NetworkHost, AutoRemove: true}, &network.NetworkingConfig{}, nil, "")
+	if err != nil {
+		return
+	}
+
+	finishChan, errChan := cli.ContainerWait(ctx, createResponse.ID, container.WaitConditionNextExit)
+
+	if err = cli.ContainerStart(ctx, createResponse.ID, container.StartOptions{}); err != nil {
+		return
+	}
+
+	select {
+	case waitReponse := <-finishChan:
+		statusCode = int(waitReponse.StatusCode)
+		if statusCode == 0 {
+			log.Println("No changes detected")
+			return
+		}
+		logOutput = GetLogs(ctx, cli, createResponse.ID)
+		log.Println("Migration changes:")
+		return
+
+	case err = <-errChan:
+		log.Println("failed to run migra", err)
+		return
+	}
 }
