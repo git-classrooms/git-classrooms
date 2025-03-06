de28923cebfd874e6e79aa87b0af29889eee9943
Fabian Petersen <fabian@nf-petersen.de>
2025-03-06 23:29:55 +0100

    fix: fix ip binding and remove unnecessary endpoint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,9 +6,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: "0.0.0.0",
     proxy: {
-      "/api": `http://${process.env.docker === "true" ? "backend" : "127.0.0.1"}:3000`,
+      "/api": `http://127.0.0.1:3000`,
     },
   },
   resolve: {
