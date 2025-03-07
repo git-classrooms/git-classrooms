7dc36d22bc91635426a0f525edf7d9c5983ee1bb
Fabian Petersen <fabian@nf-petersen.de>
2025-03-07 11:48:14 +0100

    fix: vite also needs to set xfwd headers

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     proxy: {
-      "/api": `http://127.0.0.1:3000`,
+      "/api": { target: `http://127.0.0.1:3000`, ws: true, xfwd: true },
     },
   },
   resolve: {
